09d81007f2665a76a2e663a872998b9040defba2
ProgramadorWebTI <programadorwebti@gmail.com>
2018-04-07 18:52:05 -0300

    Correção do MY_Controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,15 +1,14 @@
 <?php
 
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MY_Controller extends CI_Controller{ 
-	     public function __construct()  {
-		 parent::__construct();
-	     $logado = $this->session->userdata('logado');
-	     if ($logado != 1)
-				redirect(base_url('/'));		
-	     }   
+class MY_Controller extends CI_Controller
+{ 
+public function __construct()
+{
+parent::__construct();
+$logado = $this->session->userdata('logado');
+if ($logado != 1)
+redirect(base_url('/'));
 }
-
-
-
-?>
+}
